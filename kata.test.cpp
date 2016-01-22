b28197a27c0f29a5a057d95ca8e0d33aab616b09
quasarqueen <bbladorn@yahoo.com>
2016-01-21 20:04:32 -0500

    Update kata.test.cpp
    more test options: buffed up code to handle up to 1000

--- kata.test.cpp
+++ kata.test.cpp
@@ -44,11 +44,87 @@ string Arab2RN(string numStr)
 {
     
     string roman;
-        if (numStr[0] == '1')  {roman.append("I");}
+    const int length = numStr.length();
+    
+    int ones=length-1;
+    int tens=length-2;
+    int huns=length-3;
+    
+    if (length==1)
+    {
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+        
+    }
+    if (length==2)
+    {
+        if (numStr[tens] == '1')  {roman.append("X");}
+        if (numStr[tens] == '2')  {roman.append("XX");}
+        if (numStr[tens] == '3')  {roman.append("XXX");}
+        if (numStr[tens] == '4')  {roman.append("XL");}
+        if (numStr[tens] == '5')  {roman.append("L");}
+        if (numStr[tens] == '6')  {roman.append("LX");}
+        if (numStr[tens] == '7')  {roman.append("LXX");}
+        if (numStr[tens] == '8')  {roman.append("LXXX");}
+        if (numStr[tens] == '9')  {roman.append("XC");}
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+    }
+    if (length==3)
+    {
+        if (numStr[huns] == '1')  {roman.append("C");}
+        if (numStr[huns] == '2')  {roman.append("CC");}
+        if (numStr[huns] == '3')  {roman.append("CCC");}
+        if (numStr[huns] == '4')  {roman.append("CD");}
+        if (numStr[huns] == '5')  {roman.append("D");}
+        if (numStr[huns] == '6')  {roman.append("DC");}
+        if (numStr[huns] == '7')  {roman.append("DCC");}
+        if (numStr[huns] == '8')  {roman.append("DCCC");}
+        if (numStr[huns] == '9')  {roman.append("CM");}
+        if (numStr[tens] == '1')  {roman.append("X");}
+        if (numStr[tens] == '2')  {roman.append("XX");}
+        if (numStr[tens] == '3')  {roman.append("XXX");}
+        if (numStr[tens] == '4')  {roman.append("XL");}
+        if (numStr[tens] == '5')  {roman.append("L");}
+        if (numStr[tens] == '6')  {roman.append("LX");}
+        if (numStr[tens] == '7')  {roman.append("LXX");}
+        if (numStr[tens] == '8')  {roman.append("LXXX");}
+        if (numStr[tens] == '9')  {roman.append("XC");}
+        if (numStr[ones] == '1')  {roman.append("I");}
+        if (numStr[ones] == '2')  {roman.append("II");}
+        if (numStr[ones] == '3')  {roman.append("III");}
+        if (numStr[ones] == '4')  {roman.append("IV");}
+        if (numStr[ones] == '5')  {roman.append("V");}
+        if (numStr[ones] == '6')  {roman.append("VI");}
+        if (numStr[ones] == '7')  {roman.append("VII");}
+        if (numStr[ones] == '8')  {roman.append("VIII");}
+        if (numStr[ones] == '9')  {roman.append("IX");}
+    }
+    if (length==4)
+    {
+        roman.append("M");
+    }
     return roman;
 }
 
-
 TEST_CASE("needs to convert an arabic number to RN","[Arab2RN]"){
     CHECK( Arab2RN("1")== "I");
+    REQUIRE( Arab2RN("4")== "IV");
+    REQUIRE( Arab2RN("9")== "IX");
+    REQUIRE( Arab2RN("40")== "XL");
+    REQUIRE( Arab2RN("90")== "XC");
 }
